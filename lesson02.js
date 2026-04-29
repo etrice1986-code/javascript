9e711df2eccf1b982448535174b86e777b5c93b0
etrice1986-code <etrice1986@gmail.com>
2026-04-30 00:16:36 +0200

    fine lesson 02

--- lesson02.js
+++ lesson02.js
@@ -1,4 +1,4 @@
-console.log("ciao")
+//console.log("ciao")
 
 //I TIPI DI DATO
 
@@ -14,19 +14,19 @@ console.log("ciao")
 
 //NUMBER:
 
-let a = 5;
-let b = 10;
-let somma = a + b;
-console.log(somma);
+//let a = 5;
+//let b = 10;
+//let somma = a + b;
+//console.log(somma);
 
-let differenze  = a - b
-console.log(differenze);
+//let differenze  = a - b
+//console.log(differenze);
 
-let multiplicazione = a * b;
-console.log(multiplicazione);
+//let multiplicazione = a * b;
+//console.log(multiplicazione);
 
-let divisione = a / b
-console.log(divisione)
+//let divisione = a / b
+//console.log(divisione)
 
 //OPERATORI MATEMATICI: + - * /  %(modulo)
 
@@ -52,16 +52,16 @@ console.log(divisione)
 // .tofixed(): arrotondare il numero alla cifra decimale che voglio. MI trasformo il dato da un numero a striga.
 
 //Dog syntax
-let number = 5.564;
+//let number = 5.564;
 //console.log( number.toFixed(2))
-console.log(typeof number .toFixed (2))
+//console.log(typeof number .toFixed (2))
 
 //String: tutto cio che abbraciato , vrappato da degli apici in programmazione, è una stringa
 
 //quanti tipi di apici esistono in js: 3   '' "" ``backtick
 // per fare la backtick:  Alt + 96
  
-console.log(typeof"ciao");
+//console.log(typeof"ciao");
 
 //Concatenazione tra stringhe
 // bec slesh n :    /n per mandare a capo una stringa
@@ -69,38 +69,93 @@ console.log(typeof"ciao");
 //console.log('ciao a tutti mi chiamo \n' + nome);
 
 //console.log(`mi chiamo ${name}  `);
-let name = "matteo"
-let str = ' ciao a tutti mi chiamo '
-let saluto = str + name;
-console.log(saluto);
+//let name = "matteo"
+//let str = ' ciao a tutti mi chiamo '
+//let saluto = str + name;
+//console.log(saluto);
 
 //Type cohertion: js si accorgersi che sono due dati diversi, non per questo va in errore ma cerca di andare avanti  e converte uno dei due dati nel tipo dell'altro
-console.log(  '5' + 5)
+//console.log(  '5' + 5)
 
 //NAN  Not a Number, valore assorbente
 
 
 //Bool: true e false
-let check = true;
-console.log(typeof check);
+//let check = true;
+//console.log(typeof check);
 
 //Undefined: rappresenta un assenza di valore
-let  a = undefined ;
-console.log(typeof a);
+//let  a = undefined ;
+//console.log(typeof a);
 
 //Null rappresenta l'assenza INTENZIONALE di un oggetto , viene definito la primitiva strutturale (object)
-let b = null;
+//let b = null;
 
 //Operatarori
 
-//Operatori unari: typeof
+//Operatori unari: typeof, incremento e decremento
 //Operatori Binari: matematici, confronto, logici
 //Operatori Ternario: condizioni
 
 // Incremento: ++
-let a = 5;
-a++
+// Decremento: --
+//let a = 5;
+//a++
 
-console.log(a);
+//console.log(a);
 
-//Decremento --
+// Confronto: mi restituisce SEMPRE un valore booliano
+
+// Uguaglianza: == non tiene in considerazione il tipo di dato, ma fa scattare la type cohertion
+//console.log( 5 == '5');
+
+// Stretta uguaglianza : === il triplo uguale, (===) non fa scattare la type cohertion
+
+// Maggiore > 
+// Minore  <
+// Maggiore-uguale >=
+// Minore-uguale   <=
+//console.log( 1 < 2 < 3);
+
+// 1 < 2 < 3
+// true < 3
+// 1 < 3
+// false -> 0
+
+//console.log(3 > 2 > 1);
+
+// 3 > 2 : true
+// true > 1
+// 1 > 1 
+
+//console.log( 5 !== '5' );
+
+//Operatori logici:  &&  ||
+
+// contesto Booleano: è particolare tipo di contesto all'interno della quale tutti tipi di dato qualunque siano, assumano un significato booleano:  Trytty  o Falsy
+
+//Trutty: numeri > 0, le stringhe non vuote, tutti gli oggetti, true
+
+//Falsy:  numeri: <= 0, le stringhe vuote, null, undefined  NAN ,false
+
+// &&: ci restitusce il primo valore falsy che trova, se non ne trova, ci restituisce  l'ultimo valore Trutty
+
+// ||: ci restituisce il primo valore trutty, ne non  ne tova  retituisce il primo valore falsy
+
+//console.log( 5 && 5 < 18);
+//let voto = 19;
+//console.log( voto > 0 && voto <= 18);
+//console.log("ciao" > 0 && 100 <= 100 || 100);
+
+//OPeratore Overloading: +
+
+//let number = 5.523;
+//console.log( number.toFixed(2) );
+
+//convertire una stringa in nemero
+
+//console.log( typeof +('5') );
+// console.log( +true)= 1
+console.log( +false)
+
+
