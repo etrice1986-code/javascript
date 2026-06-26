7c0c53f9c3e244f9a96fde045e248631ef1f91ab
etrice1986-code <etrice1986@gmail.com>
2026-06-26 12:04:57 +0200

    concluso selfwork dati strutturali

---
+++ Array/array1/script.js
@@ -0,0 +1,61 @@
+
+
+
+
+let array_1 = [
+  ['un', 'per', 'incatenarli.'],
+  ['Anello', 'trovarli,'],
+  ['ghermirli', 'e'],
+  ['gondor', 'mark'],
+];
+
+let array_2 = [
+  [['trovarli,']],
+  ['tu,', 'sciocchi'],
+  ['tu,', 'sciocchi', ['padron', 'Sauron']],
+  ['nel', ['fuggite', 'gandalf']],
+  [['domarli,', 'passare'], 'buio']
+];
+
+// // Estrarre le parole utili
+// let un = array_1[0][0];
+// let Anello = array_1[1][0];
+// let per = array_1[0][1];
+
+// let domarli = array_2[4][0][0];
+// let trovarli = array_1[1][1];
+// let ghermirli = array_1[2][0];
+// let e = array_1[2][1];
+
+// let nel = array_2[3][0];
+// let buio = array_2[4][1];
+// let incatenarli = array_1[0][2];
+
+// // Costruzione della frase
+// let frase = `${un} ${Anello} ${per} ${domarli} ${un} ${Anello} ${per} ${trovarli} ${un} ${Anello} ${per} ${ghermirli} ${e} ${nel} ${buio} ${incatenarli}`;
+
+// console.log(frase);
+
+console.log(
+
+  array_1 [0][0],
+  array_1 [1][0],
+  array_1 [0][1],
+  array_2 [4][0][0],
+  array_1 [0][0],
+  array_1 [1][0],
+  array_1 [0][1],
+  array_1 [1][1],
+  array_1 [0][0],
+  array_1 [1][0],
+  array_1 [0][1],
+  array_1 [2][0],
+  array_1 [2][1],
+  array_2 [3][0],
+  array_2 [4][1],
+  array_1 [0][2]
+
+
+
+
+);
